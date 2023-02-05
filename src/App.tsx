e457a4468427765e8be3b529959e34e3ad465393
Nikita Kolotenko <tofeee63@gmail.com>
2023-02-05 17:08:50 +0200

    test socket client

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-import './App.css';
+import { useEffect } from 'react';
+import { io } from 'socket.io-client';
+const socket = io('http://localhost:8081');
 
-function App() {
+const onConnect = () => {
+	console.log('connect to socked');
+
+	socket.on('rooms:all', (arg) => {
+		console.log('room:all data:', arg);
+	});
+};
+
+const App: React.FC = () => {
+	useEffect(() => {
+		socket.on('connect', onConnect);
+
+		// return socket.removeAllListeners;
+	}, []);
+
 	return <div className="App"></div>;
-}
+};
 
 export default App;
